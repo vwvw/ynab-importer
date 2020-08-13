344f4e44baa390ab10f915b13637d348356354d5
James Cole <james@firefly-iii.org>
2020-08-13 20:30:27 +0200

    Update libraries and version.

--- config/ynab.php
+++ config/ynab.php
@@ -29,7 +29,7 @@
 declare(strict_types=1);
 
 return [
-    'version'         => '2.0.0',
+    'version'         => '2.0.1',
     'access_token'    => env('FIREFLY_III_ACCESS_TOKEN', ''),
     'uri'             => env('FIREFLY_III_URI', ''),
     'vanity_uri'      => envNonEmpty('VANITY_URI'),
